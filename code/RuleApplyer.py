9fb739ae9b42bd488aabd8b6504059c449c74e77
Sunnoogo77 <sunnoogo-caleb.tapsoba@efrei.net>
2025-01-29 20:19:13 +0000

    29-01

--- code/RuleApplyer.py
+++ code/RuleApplyer.py
@@ -2,143 +2,215 @@
 #Ici, nous définissons toutes les fonctions qui seront utilisées pour créer une liste de mots personnalisée basée sur une liste existante de mots, de chiffres et de caractères très proches du mot de passe.
 
 
-def nothingString(curString :str) -> str:
+def nothingString(curString: str) -> str:
+    # No transformation
     return curString
 
-def lowerString(curString :str) -> str:
+def lowerString(curString: str) -> str:
+    # Convert to lowercase
     return curString.lower()
 
-def upperString(curString :str) -> str:
+def upperString(curString: str) -> str:
+    # Convert to uppercase
     return curString.upper()
 
-def capitilizeString(curString :str) -> str:
+def capitilizeString(curString: str) -> str:
+    # Capitalize the first letter
     return curString.capitalize()
 
-def invertCapitalize(curString : str) -> str:
-    if len(curString) <2:
+def invertCapitalize(curString: str) -> str:
+    # Invert capitalization
+    if len(curString) < 2:
         return curString.lower()
     c = curString[0]
     curString = curString[1:]
     c = str(c).lower()
     curString = curString.upper()
-    return c+ curString
+    return c + curString
 
-def charToInt(char :chr) -> int:
+def charToInt(char: chr) -> int:
+    # Convert character to integer
     n = 0
-    if(char>='0' and char <='9'):
-        n = ord(char)-ord('0')
-    elif((char>='A' and char<='Z')):
-        n = ord(char)-ord('A')+10
-    elif((char>='a' and char<='z')):
-        n = ord(char)-ord('a')+36
+    if '0' <= char <= '9':
+        n = ord(char) - ord('0')
+    elif 'A' <= char <= 'Z':
+        n = ord(char) - ord('A') + 10
+    elif 'a' <= char <= 'z':
+        n = ord(char) - ord('a') + 36
     return n
 
-
-def toggleString(curString :str) -> str:
+def toggleString(curString: str) -> str:
+    # Toggle case of all characters
     return curString.swapcase()
 
-def toggleStringAtPos(curString :str, p :chr) ->str:
+def toggleStringAtPos(curString: str, p: chr) -> str:
+    # Toggle case at a specific position
     pos = charToInt(p)
     c = list(curString)
-    if(pos < len(curString)):
-        if(c[pos]>='a' and c[pos]<='z'):
+    if pos < len(curString):
+        if 'a' <= c[pos] <= 'z':
             c[pos] = c[pos].upper()
-        elif(c[pos]>='A' and c[pos]<='Z'):
+        elif 'A' <= c[pos] <= 'Z':
             c[pos] = c[pos].lower()
-    return"".join(c)
+    return "".join(c)
 
-def reverseString(curString :str) -> str:
+def reverseString(curString: str) -> str:
+    # Reverse the string
     return curString[::-1]
 
-def duplicateString(curString :str) -> str:
+def duplicateString(curString: str) -> str:
+    # Duplicate the string
     return curString + curString
 
-
-def duplicateStringNtimes(curString :str, n: chr) -> srt:
+def duplicateStringNtimes(curString: str, n: chr) -> str:
+    # Duplicate the string N times
     num = charToInt(n)
-    return curString*num
+    return curString * num
 
-def reflectString(curString :str,) -> str:
-    return curString +reverseString(curString)
+def reflectString(curString: str) -> str:
+    # Reflect the string
+    return curString + reverseString(curString)
 
-def rotateLeftString(curString :str,) -> str:
-    if(len(curString)>2):
+def rotateLeftString(curString: str) -> str:
+    # Rotate the string to the left
+    if len(curString) > 1:
         c = curString[0]
-        curString = curString[1]
-        curString += c
+        curString = curString[1:] + c
     return curString
 
-def rotateRightString(curString :str) -> str:
-    c = curString
-    if(len(curString)>2):
+def rotateRightString(curString: str) -> str:
+    # Rotate the string to the right
+    if len(curString) > 1:
         c = curString[-1]
-        curString =curString[:-1]
-        c += curString
-    return c
-
-def appendCharacter(curString :str, char :chr) ->str:
-    curString += str(char)
+        curString = c + curString[:-1]
     return curString
 
-def prependCharacter(curString :str, char :chr) ->str:
-    curString = str(char) + curString
-    return curString
+def appendCharacter(curString: str, char: chr) -> str:
+    # Append a character
+    return curString + str(char)
 
-def truncateRight(curString :str) -> str:
-    if(len(curString)<2):
+def prependCharacter(curString: str, char: chr) -> str:
+    # Prepend a character
+    return str(char) + curString
+
+def truncateRight(curString: str) -> str:
+    # Truncate the string from the right
+    if len(curString) < 2:
         return ""
     return curString[:-1]
 
-def deleteAtPos(curString :str, p :chr) -> str:
+def deleteAtPos(curString: str, p: chr) -> str:
+    # Delete character at a specific position
     pos = charToInt(p)
-    if(pos < len(curString)):
-        leftString = curString[:pos]
-        if(pos != len(curString) -1):
-            rightString = curString[pos+1:]
-        else:
-            rightString = ""
-        curString = leftString + rightString
+    if pos < len(curString):
+        curString = curString[:pos] + curString[pos+1:]
     return curString
 
-def extractSubstring(curString :str, startPos :chr, endPos :chr) -> str:
+def extractSubstring(curString: str, startPos: chr, endPos: chr) -> str:
+    # Extract a substring
     sPos = max(0, charToInt(startPos))
     ePos = min(charToInt(endPos), len(curString))
-    if(sPos>ePos):
-        raise Exception("Error Starting positon cannot come at ending Position")
+    if sPos > ePos:
+        raise Exception("Error: Starting position cannot come after ending position")
     return curString[sPos:ePos]
 
-def omitSubstring(curString :str, startPos :chr, endPos :chr) -> str:
+def omitSubstring(curString: str, startPos: chr, endPos: chr) -> str:
+    # Omit a substring
     sPos = charToInt(startPos)
-    ePos = charToInt(endPos)+1
-    if(sPos>ePos):
+    ePos = charToInt(endPos) + 1
+    if sPos > ePos:
         return curString
-    if(sPos<ePos):
-        raise Exception("Error Starting position cannot cme after ending Position")
-    leftString = ""
-    rightString = ""
-    if(sPos > 0):
-        leftString = curString[:sPos]
-    if(ePos < len(curString)):
-        rightString = curString[ePos:]
-    return leftString + rightString
+    if sPos < ePos:
+        raise Exception("Error: Starting position cannot come after ending position")
+    return curString[:sPos] + curString[ePos:]
 
-def insertCharacterAtPos(curString :str, p :chr, char :chr) -> str:
+def insertCharacterAtPos(curString: str, p: chr, char: chr) -> str:
+    # Insert a character at a specific position
     pos = charToInt(p)
-    return curString[:p] + str(char) + curString[p:] if 0 <= pos < len(curString) else  curString
+    return curString[:pos] + str(char) + curString[pos:] if 0 <= pos < len(curString) else curString
 
-def overwriteCharacterAtPos(curString :str, p :chr, char :chr) -str:
+def overwriteCharacterAtPos(curString: str, p: chr, char: chr) -> str:
+    # Overwrite character at a specific position
     pos = charToInt(p)
-    if(pos < len(curString)):
-        leftString = curString[:pos]
-        if(pos != len()-1):
-            rightString = curString[pos+1:]
-        else:
-            rightString = ""
-        curString = leftString + str(char) + rightString
+    if pos < len(curString):
+        curString = curString[:pos] + str(char) + curString[pos+1:]
     return curString
 
-def truncateFromPos(curString :str, p :chr) -> str:
+def truncateFromPos(curString: str, p: chr) -> str:
+    # Truncate the string from a specific position
     pos = charToInt(p)
-    if(pos < len(curString)):
+    if pos < len(curString):
         curString = curString[:pos]
     return curString
+
+def replaceCharacter(curString: str, oldChar: chr, newChar: chr) -> str:
+    # Replace a character
+    return curString.replace(oldChar, newChar)
+
+def purgeString(curString: str, char: chr) -> str:
+    # Remove all instances of a character
+    return curString.replace(char, "")
+
+def duplicateFirst(curString: str) -> str:
+    # Duplicate the first character
+    return curString[0] + curString
+
+def duplicateFirstNChars(curString: str, n: chr) -> str:
+    # Duplicate the first N characters
+    num = charToInt(n)
+    if num < len(curString):
+        curString = curString[:num] + curString
+    return curString
+
+def duplicateLast(curString: str) -> str:
+    # Duplicate the last character
+    return curString + curString[-1]
+
+def duplicateAll(curString: str) -> str:
+    # Duplicate all characters
+    return "".join([c*2 for c in curString])
+
+#Rule list
+# Dictionary mapping rule symbols to their corresponding functions
+ruleList = {
+    ":": nothingString,          # No transformation
+    "l": lowerString,            # Convert to lowercase
+    "u": upperString,            # Convert to uppercase
+    "c": capitilizeString,       # Capitalize the first letter
+    "C": invertCapitalize,       # Invert capitalization
+    "t": toggleString,           # Toggle case of all characters
+    "T": toggleStringAtPos,      # Toggle case at a specific position
+    "r": reverseString,          # Reverse the string
+    "d": duplicateString,        # Duplicate the string
+    "D": duplicateStringNtimes,  # Duplicate the string N times
+    "R": reflectString,          # Reflect the string
+    "L": rotateLeftString,       # Rotate the string to the left
+    "R": rotateRightString,      # Rotate the string to the right
+    "a": appendCharacter,        # Append a character
+    "p": prependCharacter,       # Prepend a character
+    "t": truncateRight,          # Truncate the string from the right
+    "D": deleteAtPos,            # Delete character at a specific position
+    "E": extractSubstring,       # Extract a substring
+    "O": omitSubstring,          # Omit a substring
+    "I": insertCharacterAtPos,   # Insert a character at a specific position
+    "O": overwriteCharacterAtPos,# Overwrite character at a specific position
+    "T": truncateFromPos,        # Truncate the string from a specific position
+    "R": replaceCharacter,       # Replace a character
+    "P": purgeString,            # Remove all instances of a character
+    "F": duplicateFirst,         # Duplicate the first character
+    "N": duplicateFirstNChars,   # Duplicate the first N characters
+    "L": duplicateLast,          # Duplicate the last character
+    "A": duplicateAll            # Duplicate all characters
+}
+
+# Dictionary mapping rule symbols to the number of arguments they require
+ruleCountList = {
+    "D": 1,  # Duplicate string N times requires 1 argument
+    "D": 2,  # Delete character at a specific position requires 1 argument
+    "T": 1,  # Toggle case at a specific position requires 1 argument
+    "E": 2,  # Extract a substring requires 2 arguments
+    "O": 2,  # Omit a substring requires 2 arguments
+    "I": 2,  # Insert a character at a specific position requires 2 arguments
+    "O": 2,  # Overwrite character at a specific position requires 2 arguments
+    "N": 1   # Duplicate the first N characters requires 1 argument
+}
---
+++ code/passwordCracker.py
@@ -0,0 +1,5 @@
+import itertools
+from RuleApplyer import *
+from urllib.request import urlopen, hashlib
+import hashlib
+import bcrypt
